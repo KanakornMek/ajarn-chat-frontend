139959d191fdafa745a9f9b283faf28eeb031f8b
KanakornMek <kanakorn.meka@gmail.com>
2024-03-24 14:54:17 +0700

    feat: Initialize project

---
+++ src/configs/routes.ts
@@ -0,0 +1,7 @@
+export const routes = {
+    HOME: '/',
+    ABOUT: '/about',
+    CONTACT: '/contact',
+    PROFILE: '/profile/:username', // Example of a dynamic route
+    // Add more routes as needed
+  };
